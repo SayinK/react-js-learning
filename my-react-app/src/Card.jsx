66a52ae0693b6a087d6ea762c866e1f4ce592162
SayinK <emily082505@gmail.com>
2024-08-09 18:42:04 -0500

    Made a card-component and added headers, footers, etc onto web page

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img  className = "card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">Emily Kim</h2>
+            <p className="card-text">Hello! My name is Emily Kim and I am a sophmore studying Computer Science at TAMU</p>
+        </div>
+    );
+}
+
+export default Card
