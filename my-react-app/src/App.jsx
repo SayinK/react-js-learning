35f3a2fdabdff8faccfd8edf4094fb7bfcbaefc7
SayinK <emily082505@gmail.com>
2024-08-12 17:27:31 -0500

    Implemented props, default props, and prop types

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,17 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './button.jsx'
+import Student from './Student.jsx'
+
+
+//Props = read-only properties that are shared between components. 
+//A parent component can send data to a child comp <Component key=value>
+
+//propTypes = a mechanism that ensures that the passed value is the correct datatype. 
+//age: PropTypes.number
+
+//defaultProps = default values for props in case they are not passed from the parent component 
+//name: "Guest"
 
 function App() {
   return(
@@ -12,6 +23,11 @@ function App() {
       <Footer></Footer>
       <Card></Card>
       <Button></Button>
+      <Student name="SpongeBob" age={30} isStudent={true}></Student>
+      <Student name="Patrick" age={42} isStudent={false}></Student>
+      <Student name="Squidward" age={50} isStudent={false}></Student>
+      <Student name='Sandy' age={27} isStudent={true}></Student>
+      <Student></Student>
     </>
   );
 }
--- my-react-app/src/button.jsx
+++ my-react-app/src/button.jsx
@@ -1,12 +1,25 @@
-import styles from './button.module.css'
-
 function Button(){
 
-    return<button className = {styles.button}>Click me</button> //this is a module way of importing styles
+    const styles = {
+        backgroundColor: "hsl(200,100%,50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+
+    return(<button style={styles}>Click me</button>); //this is a inline style (not great for complex styling)
 }
 
 export default Button;
 
 
-//Module Way: return<button className = {styles.button}>Click me</button>
-//
+//Exporting CSS in three ways:
+
+//Module Way: return<button className = {styles.button}>Click me</button> (dont need to worry about naming conflicts, must make
+//deicated folder for the element, make file named button.module.css and export and import styles from './')
+
+//External Way: return<button className = "button">Click me</button> ('button' comes from index.css)
+
+//Inline style: is above
